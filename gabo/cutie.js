686cb711fd9991b2fb399636e8bae6c8f48b4455
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-03-08 21:31:52 -0500

    Update cutie.js

--- gabo/cutie.js
+++ gabo/cutie.js
@@ -1,1 +1,21 @@
-$(eval const chz=['kaestr1Cool','kaestr1Hug','maizCool','abbybaCool','smolCool','lycelCool','lycelYes','therea1896Cool','smolCheese','abbybaHug','elizab34Uwu','pcrowHug'];o="Did y'all know ";end=" smolHands whyyy??";i=decodeURIComponent("$(querystring)");if(i.length==0){	o+='no one';}else{	o+=i;	end=' I bet ya didnt! '+chz[Math.floor(Math.random()*chz.length)];}o+=" is a hecking "; if(/Kae/i.test(i)){	o+='cute lil flower maizBlush ??';}else{	o+='cutie??';}o+=end;o;)
+const cheese=['  kaestr1Cool    ',
+              '  kaestr1Hug     ',
+              '  maizCool       ',
+              '  abbybaCool     ',
+              '  smolCool       ',
+              '  lycelCool      ',
+              '  lycelYes       ',
+              '  therea1896Cool ',
+              '  smolCheese     ',
+              '  abbybaHug      ',
+              '  elizab34Uwu    ',
+              '  pcrowHug       '];
+
+o="Did y'all know ";
+end=" smolHands whyyy??";
+if(i.length==0){o+='no one';}
+else{	o+=i;	end=' I bet ya didnt! '+chz[Math.floor(Math.random()*chz.length)];}
+o+=" is a hecking "; 
+if(/Kae/i.test(i)){	o+='cute lil flower maizBlush ??';}
+else{	o+='cutie??';}
+o+=end;o;)
