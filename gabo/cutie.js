ba3483a0a54e42cd70ee10fc98c403685cfbcd6a
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-03-08 21:24:33 -0500

    Update cutie.js

--- gabo/cutie.js
+++ gabo/cutie.js
@@ -1,5 +1,1 @@
-this
-is
-a
-test
-YEP
+$(eval const chz=['kaestr1Cool','kaestr1Hug','maizCool','abbybaCool','smolCool','lycelCool','lycelYes','therea1896Cool','smolCheese','abbybaHug','elizab34Uwu','pcrowHug'];o="Did y'all know ";end=" smolHands whyyy??";i=decodeURIComponent("$(querystring)");if(i.length==0){	o+='no one';}else{	o+=i;	end=' I bet ya didnt! '+chz[Math.floor(Math.random()*chz.length)];}o+=" is a hecking "; if(/Kae/i.test(i)){	o+='cute lil flower maizBlush ??';}else{	o+='cutie??';}o+=end;o;)
